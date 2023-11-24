aba2a625b91fb6842a455b91061a2d752868a8d9
unknown <ipanirtiano@gmail.com>
2023-11-24 22:24:47 +0700

    update client side home II

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -13,7 +13,7 @@ const Page = () => {
 
   const getAllPostById = async () => {
     try {
-      const response = await axios.get("/api/post/me");
+      const response = await axios.get("/api/post");
       setDataPost(response.data.data);
       return response.data.data;
     } catch (error) {
